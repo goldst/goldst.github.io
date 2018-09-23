117bf1c45543022be4e99581d3eef499a06cfe3d
Leonard Goldstein <leonard-goldstein@outlook.de>
2018-09-24 00:05:33 +0200

    deleting unnecessary node stuff

--- node_modules/maid/lib/MaidError.js
+++
@@ -1,6 +0,0 @@
-module.exports = class MaidError extends Error {
-  constructor(msg) {
-    super(msg)
-    this.name = 'MaidError'
-  }
-}
--- node_modules/maid/lib/loadFile.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-const JoyCon = require('joycon')
-
-module.exports = new JoyCon({
-  // Stop reading at parent dir
-  // i.e. Only read file from process.cwd()
-  stopDir: path.dirname(process.cwd())
-})
