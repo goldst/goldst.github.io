bb032e5e351f118c43afcc0cd5a259676c604989
Leonard Goldstein <leonard-goldstein@outlook.de>
2018-09-25 17:28:41 +0200

    js documentation updates 2: mainly CardAbstract.js

--- blocks/card/CardAbstract.js
+++ blocks/card/CardAbstract.js
@@ -20,10 +20,23 @@ export default class cardAbstract extends TC {
 
         console.log(this);
     }
-
+https://travis-ci.com/goldst/ein-web.svg?token=LoKS6SaGaZPsK3WPmufe&branch=master
+    /**
+     * function which returns the correct transformation for a standard
+     * card block depending on the absolute position on screen and the
+     * mouse position
+     * @protected
+     * @see {TransformationFunctions}
+     * @param {number[]} absoluteOrigin - absolute transformation origin
+     * @param {number[]} mousePosition - absolute mouse position
+     * @returns transformation with adjusted scale and 3d rotation
+     */
     _transformationFunction(absoluteOrigin, mousePosition) {
         const rotAxis = LA.vector(absoluteOrigin, mousePosition),
-            rotation = TF.advBellCurve(absoluteOrigin, mousePosition, 0, 1, 2) - 1,
+            rotation = 
+                TF.advBellCurve(
+                    absoluteOrigin, mousePosition, 0, 1, 2
+                ) - 1,
             invRotAxis = [
                 rotAxis[1],
                 -rotAxis[0]
@@ -38,12 +51,31 @@ export default class cardAbstract extends TC {
                'translateZ(0) ';
     }
 
-    /*abstract*/ _postTransformFunction(event, element) {
+    /**
+     * in any implementation, override this function with a function that
+     * does what should happen after transforming the element (eg. 
+     * setting the correct background to the parent element)
+     * @see {PostTransformFunctions}
+     * @abstract
+     * @protected
+     * @param {event} event - event that caused the transformation
+     * @param {TransformableElement} element - element that was
+     *   transformed
+     * @returns {void}
+     */
+    _postTransformFunction(event, element) {
         //look for implementation examples in
         //  'js/transformation/TransformableElement/PostTransformFunctions.js'
         this._throwIfAbstractFunction();
     }
 
+    /**
+     * more specific version of {TransformationController}'s
+     * mousemoveEventTransform that already specifies where to find the
+     * passed transformation functions and what elements it applies to
+     * @see TransformationController
+     * @returns {void}
+     */
     mousemoveEventTransform() {
         super.mousemoveEventTransform(
             this._transformationFunction,
@@ -52,12 +84,28 @@ export default class cardAbstract extends TC {
         );
     }
 
+    /**
+     * helper function that throws if class name ends with 'Abstract'
+     * @private
+     * @throws {Error} 'cannot call functions in abstract class'
+     */
     _throwIfAbstractClass() {
         if((typeof new.target).endsWith('Abstract')) {
             throw new Error('cannot call functions in abstract class');
         }
     }
 
+    /**
+     * Helper function that always throws an error. Override the method
+     * which calls this to avoid that.
+     * @private
+     * @throws {Error} 'cannot call functions in abstract class' if class
+     *   name ends on 'Abstract'
+     * @throws {Error} 'cannot call function in child class of abstract
+     *   class that does not override the abstract function' in any other
+     *   case
+     * @returns {void}
+     */
     _throwIfAbstractFunction() {
         this._throwIfAbstractClass();
 
