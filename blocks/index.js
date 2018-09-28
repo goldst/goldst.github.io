44813b07dfb29b66efcd5a432f45219ee4de0d20
Leonard Goldstein <leonard-goldstein@outlook.de>
2018-09-29 01:14:18 +0200

    project cleaning, remove unnecesseary files, especially js, which fixes hover shadow effects

--- blocks/index.js
+++ blocks/index.js
@@ -1,11 +1,9 @@
-import mainContainer from './main-container/main-container.js';
 import './card/card.js';
 import mainCard from './main-card/main-card.js';
 import projects from './projects/projects.js';
 import projectCard from './project-card/project-card.js';
 import tags from './tags/tags.js';
 
-mainContainer();
 mainCard();
 projects();
 projectCard();
--- blocks/main-card/main-card.js
+++ blocks/main-card/main-card.js
@@ -4,53 +4,8 @@
  * @returns {void}
  */
 export default () => {
-    enableMainCardToggle();
-    //mousemoveTransformations();
     setMouseoverBackgroundVars();
 };
-
-/**
- * @returns {void}
- */
-function enableMainCardToggle() {
-    document.querySelector('.main-card__toggle')
-        .addEventListener("click", toProjects);
-}
-
-/**
- * @param {event} e - event that contains a checkbox target
- * @returns {void}
- */
-function toProjects(e) {
-    const mainCard = document.querySelector('.main-card');
-
-    if(e.target.checked) {
-        location.href = '#projects';
-        mainCard.classList.remove('main-card--initial');
-        mainCard.classList.toggle('main-card--text-invisible');
-        mainCard.classList.toggle('main-card--minimized');
-    } else {
-        location.href = '#';
-        mainCard.classList.toggle('main-card--minimized');
-        setTimeout(()=>{
-            mainCard.classList.toggle('main-card--text-invisible');
-        }, 500);
-    }
-
-    //remove focus from button
-    document.querySelector(':focus').blur();
-}
-
-/*function mousemoveTransformations() {
-    const tc = new TransformationController(
-        document.querySelector('.main-container')
-    );
-
-    tc.mousemoveEventTransform(
-        TF.combined3d,
-        '.main-card--depth'
-    );
-}*/
 
 /**
  * @returns {void}
