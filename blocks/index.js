5a28ab07525d5586ecda9a7188234ba16f110d0a
Leonard Goldstein <leonard-goldstein@outlook.de>
2020-02-25 23:11:17 +0100

    background mouse shadow

--- blocks/index.js
+++ blocks/index.js
@@ -1,5 +1,6 @@
 import './card/card.js';
 import './button/button.js';
+import './page/Page.js';
 import projects from './projects/projects.js';
 import projectCard from './project-card/project-card.js';
 import tags from './tags/tags.js';
---
+++ blocks/page/Page.js
@@ -0,0 +1,63 @@
+import MC from '../../js/mouseShadow/MouseShadowController.js';
+
+/**
+ * Base for the mouse shadow controller for button blocks, which automates
+ * the mouse shadow effect
+ * @abstract
+ * @extends MouseShadowController
+ */
+class Page extends MC {
+    /**
+     * @param {String} [modifier = ''] Modifier of the button block.
+     *   Default '' means mo modifier.
+     * @returns {void}
+     */
+    constructor() {
+        super(document.getElementsByTagName('html')[0], ['page'], '.page');
+    }
+
+    /**
+     * more specific version of {MouseShadowController}'s
+     * mousemoveEventShadow that already specifies where to find the
+     * passed shadow functions and what elements it applies to
+     * @see MouseShadowController
+     * @returns {void}
+     */
+    mousemoveEventShadow() {
+        super.mousemoveEventShadow(
+            this._shadowFunction,
+            '*',
+            this._postShadowFunction
+        );
+    }
+
+    /**
+     * function which takes an object args which contains e mousemove
+     * event (event) and returns css changes
+     * @param {object} args
+     * @returns {object} object of css properties and their values
+     */
+    _shadowFunction(args) {
+        const
+            rect = args.eventControlElement.domElement.getBoundingClientRect(),
+            hover = [
+                args.event.clientX - rect.left,
+                args.event.clientY - rect.top
+            ];
+
+        return {
+            '--page--mouse-shadow-left': hover[0],
+            '--page--mouse-shadow-top': hover[1],
+            '--page--center': Math.max(
+                Math.abs(args.event.clientX - 0.5 * rect.width), 
+                Math.abs(args.event.clientY - 0.5 * rect.height)),
+            '--page--center-v': Math.abs(args.event.clientY - 0.5 * rect.height)
+        };
+    }
+
+    _postShadowFunction() {
+
+    }
+}
+
+new Page().mousemoveEventShadow();
