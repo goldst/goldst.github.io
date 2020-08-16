c583dc5945dfe747d681bc5d8ce15cafb6659c62
Leonard Goldstein <leonard-goldstein@outlook.de>
2020-08-16 14:53:34 +0200

    add scroll project card transform

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -2,7 +2,6 @@ import Card from './Card.js';
 import CardMain from './Card--main.js';
 import CardProjects from './Card--projects.js';
 
-
 window.addEventListener('load', () => {
     const
         CP = new CardProjects(),
@@ -13,22 +12,36 @@ window.addEventListener('load', () => {
     CM.mousemoveEvent();
     C.mousemoveEvent();
 
-    window.addEventListener('mousemove', (e) => {
-        CP.doEvent(e);
-        CM.doEvent(e);
-        // TODO: do i need this? In which case?
-        // C.doEvent(e);
-    });
-
     /**
      * flushes all caches that can be flushed
      * @returns {void}
      */
-    const onMove = () => {
+    function onMove() {
         CP.flushRectCaches();
         CM.flushRectCaches();
         //C.flushRectCaches();
-    };
+    }
+    
+    let lastEvent = null;
+    function doEvent(event = lastEvent) {
+        CP.doEvent(event);
+        CM.doEvent(event);
+        // TODO: do i need this? In which case?
+        // C.doEvent(event);
+    }
+
+    window.addEventListener('mousemove', (e) => {
+        lastEvent = e;
+        doEvent();
+    });
+
+    /**
+     * @todo this creates a dependency to the projects block by using
+     *   their classes, resolve somehow?
+     */ 
+    document.querySelector('.projects__inner').addEventListener('scroll', (e) => {
+        doEvent();
+    });
 
     window.addEventListener('pushstate', onMove);
     window.addEventListener('popstate', onMove);
