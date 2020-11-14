4f37a25b64179db0af10b6b7e6f4b53bfdde1deb
Leonard Goldstein <leonard-goldstein@outlook.de>
2020-11-14 16:00:05 +0100

    prepare for bemdoc-gen release

---
+++ test/js/mouseShadow/MouseShadowController.js.test.js
@@ -0,0 +1,4 @@
+const MouseShadowController = require('./../../../js/mouseShadow/MouseShadowController.js').default;
+const assert = require('chai').assert;
+describe('js/mouseShadow/MouseShadowController.js', function() {
+});
